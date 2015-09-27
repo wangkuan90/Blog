f908bd567e9619ef28630a281cdc6fe6b4a679e0
wangkuan <wangkuan@guahao.com>
2015-09-27 21:00:14 +0800

    首页部分提交 ——wk

--- jsdev/index.js
+++ jsdev/index.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
 
 	var $ = require('jquery');
+	var header = require("header");
 	
 });
--- jsdev/main.js
+++ jsdev/main.js
@@ -3,7 +3,8 @@ seajs.config({
 		"jquery": "jquery/jquery-sea-2.1.4.min.js",
 		"$": "jquery/jquery-sea-2.1.4.min.js",
 		"livereload": "http://127.0.0.1:35729/livereload",
-		"index": "./jsdev/index"
+		"index": "./jsdev/index",
+		"header" : "./common/header"
 	},
 	// 预加载项
 	preload: [
